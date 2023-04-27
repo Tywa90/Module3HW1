d4c62df4c90b02774f6b5638c33758eb44f5adfa
Serhii Amelin <amserega90@gmail.com>
2023-04-27 17:28:07 +0300

    New day new project

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
-﻿namespace WorkWithList
+﻿using System.Net.WebSockets;
+
+namespace WorkWithList
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Starter starter = new Starter();
-            starter.Run();
+            // Starter starter = new Starter();
+            // starter.Run();
+            var cars = new CarsT<string>();
+
+            cars.Add("Tesla");
+            cars.Add("Jeep");
+            cars.Add("Ford");
+            cars.Add("Audi");
+
+            cars.AddRange(new string[] { "Subaru", "Toyota", "Nissan", "MAN", "SEAT", "Skoda", "Bentley", "Bugatti", "Lamborghini" });
+
+
         }
     }
 }
